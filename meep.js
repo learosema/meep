cdf4ffd4a284b9295005c1a096d0a3ebe555e2f3
Lutz Rosema <info@terabaud.de>
2015-06-15 16:30:33 +0200

    changed a's to div's

--- meep.js
+++ meep.js
@@ -1,49 +1,38 @@
-var ac = new AudioContext()
+w=window
+d=document
+function $(s,n){return(n||d).querySelector(s)}
+function $$(s,n){return[].slice.call((n||d).querySelectorAll(s))}
+ac=new AudioContext()
 P=[]
-//mini-jQuery ;)
-function $(s,n){return(n||document).querySelector(s)}
-function $$(s,n){return[].slice.call((n||document).querySelectorAll(s))}
-
-function mouseHandler(e){
-	var li,a,b=0,i
-	if(e.buttons==0)return
-	if(e.target.nodeName=='A') {
-		li=e.target.parentNode
-		a=e.target
-	}
-	if (e.target.nodeName=='LI') {
-		li=e.target
-		a=$('a',li)
-		b=1
-	}
+function mouse(e) {
+	var li,b=0,i
+	if(e.buttons==0||e.target.nodeName=='UL')return
+	if(e.target.nodeName=='DIV')li=e.target.parentNode
+	if(e.target.nodeName=='LI'){li=e.target;b=1}
 	i=$$('.piano li').indexOf(li)
 	i=(i/7|0)*12+("0x"+("024579b"[i%7])|0)+b
 	if(P[i])return
 	P[i]=note(i)
-	var off=function () {
-    	if (P[i])P[i].stop()
-    	P[i]=void 0
-		H.map(function(h){window.removeEventListener(h)})		
-    }
-    var H=['mouseup','mouseout','dragend'].map(function(h){return window.addEventListener(h, off)})
+	var of=function(){
+		if (P[i])P[i].stop()
+		P[i]=null
+		H.map(function(h) {
+			w.removeEventListener(h)
+		})
+	}
+	var H=['mouseup','mouseout','dragend'].map(function(h){
+		return w.addEventListener(h,of)
+	})
 }
-
-['mousedown','mouseover','dragstart'].map(function(e){$('.piano').addEventListener(e, mouseHandler)})
-
-
-function note(i,g) {
-	var osc = ac.createOscillator()
-    var gain = ac.createGain()
-    osc.connect(gain)
-    gain.connect(ac.destination)
-    osc.type = "sawtooth"
-    osc.frequency.value = 264*Math.pow(2,i/12)
-    gain.gain.value = g || 1.0
-    osc.start()
-    return osc;
+function note(i) {
+	var O=ac.createOscillator()
+	O.connect(ac.destination)
+	O.type="sawtooth"
+	O.frequency.value=264*Math.pow(2,i/12)
+	O.start()
+	return O
 }
-
-window.onkeydown = function (e) {
+w.onkeydown=function(e) {
 	console.log(e)
-}
-
+};
+['mousedown','mouseover','dragstart'].map(function(e){$('.piano').addEventListener(e,mouse)})
