653b42ddf548a695962e6b464d60d3ce26f080b1
Lutz Rosema <info@terabaud.de>
2015-06-15 15:56:31 +0200

    Added meepy sawtooth tone

--- meep.js
+++ meep.js
@@ -1,24 +1,49 @@
-var ac = new AudioContext();
-function $(s,n){return (n || document).querySelector(s)}
-function $$(s,n){return [].slice.call((n || document).querySelectorAll(s))}
-console.log($('.piano'));
-$('.piano').onclick = function (e) {
-	console.log(e.target)
-	var li, a, b=0, i;
-	if (e.target.nodeName === 'A') {
-		li = e.target.parentNode;
-		a = e.target
+var ac = new AudioContext()
+P=[]
+//mini-jQuery ;)
+function $(s,n){return(n||document).querySelector(s)}
+function $$(s,n){return[].slice.call((n||document).querySelectorAll(s))}
+
+function mouseHandler(e){
+	var li,a,b=0,i
+	if(e.buttons==0)return
+	if(e.target.nodeName=='A') {
+		li=e.target.parentNode
+		a=e.target
 	}
-	if (e.target.nodeName === 'LI') {
-		li = e.target;
-		a = $('a', li)
-		b = 1;
+	if (e.target.nodeName=='LI') {
+		li=e.target
+		a=$('a',li)
+		b=1
 	}
-	i = $$('.piano li').indexOf(li);
-	console.log(i);
-	playNote(i*2+b);
+	i=$$('.piano li').indexOf(li)
+	i=(i/7|0)*12+("0x"+("024579b"[i%7])|0)+b
+	if(P[i])return
+	P[i]=note(i)
+	var off=function () {
+    	if (P[i])P[i].stop()
+    	P[i]=void 0
+		H.map(function(h){window.removeEventListener(h)})		
+    }
+    var H=['mouseup','mouseout','dragend'].map(function(h){return window.addEventListener(h, off)})
 }
 
-function playNote(i) {
-	console.log(i);
+['mousedown','mouseover','dragstart'].map(function(e){$('.piano').addEventListener(e, mouseHandler)})
+
+
+function note(i,g) {
+	var osc = ac.createOscillator()
+    var gain = ac.createGain()
+    osc.connect(gain)
+    gain.connect(ac.destination)
+    osc.type = "sawtooth"
+    osc.frequency.value = 264*Math.pow(2,i/12)
+    gain.gain.value = g || 1.0
+    osc.start()
+    return osc;
 }
+
+window.onkeydown = function (e) {
+	console.log(e)
+}
+
