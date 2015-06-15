19784499d0adb3c59d294d54fbda7bea682a70d0
Lutz Rosema <info@terabaud.de>
2015-06-15 12:55:13 +0200

    added click event

--- meep.js
+++ meep.js
@@ -1,4 +1,24 @@
 var ac = new AudioContext();
-window.onclick = function (e) {
-	console.log(e);
+function $(s,n){return (n || document).querySelector(s)}
+function $$(s,n){return [].slice.call((n || document).querySelectorAll(s))}
+console.log($('.piano'));
+$('.piano').onclick = function (e) {
+	console.log(e.target)
+	var li, a, b=0, i;
+	if (e.target.nodeName === 'A') {
+		li = e.target.parentNode;
+		a = e.target
+	}
+	if (e.target.nodeName === 'LI') {
+		li = e.target;
+		a = $('a', li)
+		b = 1;
+	}
+	i = $$('.piano li').indexOf(li);
+	console.log(i);
+	playNote(i*2+b);
+}
+
+function playNote(i) {
+	console.log(i);
 }
