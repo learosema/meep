13e7c7d30f1af2017da3423c9be2420be4c659fc
Lutz Rosema <info@terabaud.de>
2015-06-15 21:53:54 +0200

    More DRY
    Combine mouse and keyboard events

--- meep.js
+++ meep.js
@@ -3,44 +3,36 @@ d=document
 $=String.fromCharCode
 p=d.querySelector('.piano')
 L=[].slice.call(p.querySelectorAll('li'))
-ac=new AudioContext()
+a=new AudioContext()
 P=[]
 m='mouse'
 K="AWSEDFTGYHUJKOLP"
 if(navigator.language=='de')K=K.replace(/Y/,'Z')
 function I(e,b,l,n){
+	if(e.keyCode)return K.indexOf($(e.keyCode))
 	l=e.target
 	b=0
-	if((n=l.nodeName)=='UL')return-1
-	if(n=='LI')b=1
-	if(n=='DIV')l=l.parentNode
+	if((l[n='nodeName'])=='LI')b=1
+	if(l[n]=='DIV')l=l.parentNode
+	if(l[n]!='LI')return-1
 	i=L.indexOf(l)
 	i=(i/7|0)*12+("0x"+("024579b"[i%7])|0)+b
 	return i
 }
 function N(i,O) {
-	O=ac.createOscillator()
-	O.connect(ac.destination)
+	O=a.createOscillator()
+	O.connect(a.destination)
 	O.type="sawtooth"
 	O.frequency.value=264*Math.pow(2,i/12)
 	O.start()
 	return O
 }
-w.onkeydown=function(e,i){
-	i=K.indexOf($(e.keyCode))
-	if(i<0||P[i])return
-	P[i]=N(i)
-}
-w.onkeyup=function(e,i){
-	i=K.indexOf($(e.keyCode))
-	if(i>-1&&P[i]){P[i].stop();P[i]=0}
-}
-for(M=[m+'down',m+'over','dragstart','touchstart'];e=M.pop();)p['on'+e]=function(e,i){
+for(M=[m+'down',m+'over','dragstart','touchstart','keydown'];e=M.pop();)w['on'+e]=function(e,i){
 	i=I(e)
-	if(e.buttons==0||i<0||P[i])return
+	if(e.buttons===0||i<0||P[i])return
 	P[i]=N(i)
 }
-for(M=[m+'up',m+'out','dragend','touchend'];e=M.pop();)p['on'+e]=function(e,i){
+for(M=[m+'up',m+'out','dragend','touchend','keyup'];e=M.pop();)w['on'+e]=function(e,i){
 	i=I(e)
 	if(i>-1&&P[i]){P[i].stop();P[i]=0}
 }
