7f234cf4b97a310a797f013adf9a38c8b31280da
Lutz Rosema <info@terabaud.de>
2015-06-15 18:15:16 +0200

    fixed bugs and improved key labels

--- meep.js
+++ meep.js
@@ -3,7 +3,9 @@ d=document
 $=String.fromCharCode
 p=d.querySelector('.piano')
 L=[].slice.call(p.querySelectorAll('li'))
-ac=new AudioContext();P=[];m='mouse'
+ac=new AudioContext()
+P=[]
+m='mouse'
 function I(e){
 	var l=e.target,b=0
 	if(l.nodeName=='UL')return-1
@@ -23,7 +25,7 @@ function N(i) {
 }
 K="AWSEDFTGYHUJKOLP"
 w.onkeydown=function(e){
-	var i=K.indexOf($(e.keyCode))
+	var i=K.indexOf($(e.keyCode).replace(/Z/,'Y'))
 	if(i<0||P[i])return
 	P[i]=N(i)
 }
@@ -35,7 +37,7 @@ w.onkeyup=function(e){
 	var i=I(e)
 	if(e.buttons==0||i<0||P[i])return
 	P[i]=N(i)
-})})
+})});
 [m+'up',m+'out','dragend','touchend'].map(function(h){w.addEventListener(h,function(e){
 	var i=I(e)
 	if(i>-1&&P[i]){P[i].stop();P[i]=0}
