d94129545b2a0505a1acd3d86c09cdb28ae7b5f2
Lutz Rosema <info@terabaud.de>
2015-06-30 21:06:29 +0200

    fixed messy indents

--- meep.js
+++ meep.js
@@ -15,7 +15,7 @@ function I(e,l,b,i,n){
 	if(l[n]=='DIV')l=l.parentNode
 	if(l[n]!='LI')return-1
 	i=L.indexOf(l)
-  i=(i/7|0)*12+("0x"+("024579b"[i%7])|0)+(b|0)
+	i=(i/7|0)*12+("0x"+("024579b"[i%7])|0)+(b|0)
 	return i
 }
 function N(i,O){
@@ -27,7 +27,7 @@ function N(i,O){
 	return O
 }
 for(M=[m+'down',m+'over','dragstart','touchstart','touchmove','keydown'];e=M.pop();)w['on'+e]=function(e,i){
-  i=I(e)
+	i=I(e)
 	if(e.buttons===0||i<0||P[i])return
 	P[i]=N(i)
 }
