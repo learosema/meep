c67ca64d5dc67f41a245c5b1f43990dcfa500356
Lutz Rosema <info@terabaud.de>
2015-06-30 20:56:48 +0200

    Updated styles, added keyboard input

--- meep.js
+++ meep.js
@@ -8,18 +8,17 @@ P=[]
 m='mouse'
 K="AWSEDFTGYHUJKOLP"
 if(navigator.language=='de')K=K.replace(/Y/,'Z')
-function I(e,b,l,n){
+function I(e,l,b,i,n){
 	if(e.keyCode)return K.indexOf($(e.keyCode))
 	l=e.target
-	b=0
 	if((l[n='nodeName'])=='LI')b=1
 	if(l[n]=='DIV')l=l.parentNode
 	if(l[n]!='LI')return-1
 	i=L.indexOf(l)
-	i=(i/7|0)*12+("0x"+("024579b"[i%7])|0)+b
+  i=(i/7|0)*12+("0x"+("024579b"[i%7])|0)+(b|0)
 	return i
 }
-function N(i,O) {
+function N(i,O){
 	O=a.createOscillator()
 	O.connect(a.destination)
 	O.type="sawtooth"
@@ -27,12 +26,13 @@ function N(i,O) {
 	O.start()
 	return O
 }
-for(M=[m+'down',m+'over','dragstart','touchstart','keydown'];e=M.pop();)w['on'+e]=function(e,i){
-	i=I(e)
+for(M=[m+'down',m+'over','dragstart','touchstart','touchmove','keydown'];e=M.pop();)w['on'+e]=function(e,i){
+  i=I(e)
 	if(e.buttons===0||i<0||P[i])return
 	P[i]=N(i)
 }
-for(M=[m+'up',m+'out','dragend','touchend','keyup'];e=M.pop();)w['on'+e]=function(e,i){
+for(M=[m+'up',m+'out','dragend','touchend','touchcancel','keyup'];e=M.pop();)w['on'+e]=function(e,i){
 	i=I(e)
 	if(i>-1&&P[i]){P[i].stop();P[i]=0}
 }
+
